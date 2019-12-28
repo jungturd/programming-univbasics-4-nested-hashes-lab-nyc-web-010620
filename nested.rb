d2f01d7995280a572c56a8441bc45ae1453b51c3
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 04:57:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,6 +86,10 @@ def adding_matz
         }
      }
 
+		 programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
     
 end
 
@@ -107,6 +111,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     
      
      
 end
