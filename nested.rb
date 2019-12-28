0969ea79368a3835f3f676148bd04504688ad758
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 04:59:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
         }
      }
      
-       programmer_hash[:alan_kay][:known_for]
+      programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
      
-    programmer_hash[:dennis_ritchie][:languages]
+      programmer_hash[:dennis_ritchie][:languages]
     
 end
 
@@ -86,10 +86,10 @@ def adding_matz
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto] = {}
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-		 programmer_hash
+      programmer_hash[:yukihiro_matsumoto] = {}
+      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+      programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+      programmer_hash
     
 end
 
@@ -111,10 +111,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
     
-    alans_new_info = "GUI"
+      alans_new_info = "GUI"
 
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
-	  programmer_hash
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
+      programmer_hash
      
 end
 
@@ -134,5 +134,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     	programmer_hash
 
 end
